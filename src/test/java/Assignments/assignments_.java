1bb037671f793b60bd3abb7b04ebe4b9b9b8b57a
DimitarStatev <Statevbg1@gmail.com>
2022-12-03 21:52:17 +0200

    test commit

--- src/test/java/Assignments/assignments_.java
+++ src/test/java/Assignments/assignments_.java
@@ -42,4 +42,33 @@ public class assignments_ {
         System.out.println("asd");
 
     }
+    @Test
+    public static void Task2(){
+
+        //Arrange
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //Act
+        //Wait for cookies and accept
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='L2AGLb']/div[@class='QS5gu sy4vM'][1]")));
+        driver.findElement(By.xpath("//button[@id='L2AGLb']/div[@class='QS5gu sy4vM'][1]")).click();
+        //Navigate to search box and enter data
+        driver.findElement(By.xpath("//input[@class='gLFyf']")).sendKeys("way2automation");
+        //Click on search button
+        driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']/center/input[@name='btnK'][@class='gNO89b']")).click();
+        //Click on first result
+        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div[1]/a/h3")).click();
+        // Store in list of WebElements all elements with tagname "a" , print number of stored elements, and text
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+        for (WebElement link: links) {
+            System.out.println(link.getText() + link.getAttribute("Href"));
+
+        }
+
+
+    }
 }
