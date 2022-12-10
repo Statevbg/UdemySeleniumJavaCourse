e68482cb297ef61115ad100bd3686a72189ad05f
DimitarStatev <Statevbg1@gmail.com>
2022-12-10 10:13:24 +0200

    test commit

--- src/test/java/Assignments/assignments_.java
+++ src/test/java/Assignments/assignments_.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.List;
 
